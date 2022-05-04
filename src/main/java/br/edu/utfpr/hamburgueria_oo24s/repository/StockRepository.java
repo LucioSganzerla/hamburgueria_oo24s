10f61056c4038cb7f98f1ec689c5bcc0a9d746f2
ValcirJr <balbinottijr@outlook.com>
2022-05-03 22:30:53 -0300

    Data manipulating for Stock and StockRegister

---
+++ src/main/java/br/edu/utfpr/hamburgueria_oo24s/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package br.edu.utfpr.hamburgueria_oo24s.repository;
+
+import br.edu.utfpr.hamburgueria_oo24s.model.Item;
+import br.edu.utfpr.hamburgueria_oo24s.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository  extends JpaRepository<Stock, Long> {
+    Stock findFirstByItem(Item item);
+}
